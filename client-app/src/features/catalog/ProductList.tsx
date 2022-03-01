e4432517c8600cc010ed926e2ed918c7a1f050b2
Mateus Torres <mateustorres67@hotmail.com>
2022-03-01 11:06:07 -0300

    ajuste de estilo

--- client-app/src/features/catalog/ProductList.tsx
+++ client-app/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { List } from "@mui/material";
+import { Grid } from "@mui/material";
 import React from "react";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
@@ -9,10 +9,12 @@ interface Props {
 
 export default function ProductList({ products }: Props) {
     return (
-        <List>
+        <Grid container spacing={4}>
             {products.map((product) => (
-                <ProductCard key={product.id} product={product}></ProductCard>
+                <Grid item xs={3} key={product.id}>
+                    <ProductCard product={product}></ProductCard>
+                </Grid>
             ))}
-        </List>
+        </Grid>
     );
 }
