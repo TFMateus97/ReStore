2359cf5cabd6ac1a0ac3e0175016ccd951c25c51
Mateus Torres <mateustorres67@hotmail.com>
2022-03-02 00:35:35 -0300

    Implementação do dark mode

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,42 +1,41 @@
-import { Container, CssBaseline } from "@mui/material";
-import { useEffect, useState } from "react";
+import {
+    Container,
+    createTheme,
+    CssBaseline,
+    ThemeProvider,
+} from "@mui/material";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./Header";
 
 function App() {
-    const [products, setProducts] = useState<Product[]>([]);
+    const [darkMode, setDarkMode] = useState(false);
+    const palleteType = darkMode ? "dark" : "light";
 
-    useEffect(() => {
-        fetch("http://localhost:5000/api/products")
-            .then((response) => response.json())
-            .then((data) => setProducts(data));
-    }, []); //Se nao usar a dependencia, esse método irá executar toda a vez que o componente renderizar
-    //[] -  significa que será chamado apenas quando o componente montar (mount)
-
-    function addProduct() {
-        setProducts((prevState) => [
-            ...prevState,
-            {
-                id: prevState.length + 101,
-                name: "product" + prevState.length + 1,
-                price: prevState.length * 100 + 100,
-                brand: "some brand",
-                description: "some description",
-                pictureUrl: "http://picsum.photos/200",
-                quantityInStock: 10,
+    const theme = createTheme({
+        palette: {
+            mode: palleteType,
+            background: {
+                default: palleteType === "light" ? "#eaeaea" : "#121212",
             },
-        ]);
+        },
+    });
+
+    function handleThemeChange() {
+        setDarkMode(!darkMode);
     }
 
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <CssBaseline></CssBaseline>
-            <Header></Header>
+            <Header
+                darkMode={darkMode}
+                handleThemeChange={handleThemeChange}
+            ></Header>
             <Container>
-                <Catalog products={products} addProduct={addProduct}></Catalog>
+                <Catalog></Catalog>
             </Container>
-        </>
+        </ThemeProvider>
     );
 }
 
--- client-app/src/features/catalog/Catalog.tsx
+++ client-app/src/features/catalog/Catalog.tsx
@@ -1,24 +1,20 @@
-import { Button } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([]);
 
-//export default function Catalog(props: Props) {
+    useEffect(() => {
+        fetch("http://localhost:5000/api/products")
+            .then((response) => response.json())
+            .then((data) => setProducts(data));
+    }, []); //Se nao usar a dependencia, esse método irá executar toda a vez que o componente renderizar
+    //[] -  significa que será chamado apenas quando o componente montar (mount)
 
-//dessa forma, nao precisa chamar props.propriedade, mas tem que especificar o parametro
-export default function Catalog({ products, addProduct }: Props) {
     return (
         <>
             <ProductList products={products}></ProductList>
-            <Button variant="contained" onClick={addProduct}>
-                {" "}
-                Add product
-            </Button>
         </>
     );
 }
