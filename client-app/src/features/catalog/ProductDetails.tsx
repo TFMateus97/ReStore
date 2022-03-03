149b580039abf1bb8ddcba4c19308df6f1efa58c
Mateus Torres <mateustorres67@hotmail.com>
2022-03-03 00:48:06 -0300

    Product Details component

--- client-app/src/features/catalog/ProductDetails.tsx
+++ client-app/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,25 @@
 import { Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
-    return <Typography variant="h2">Product details page</Typography>;
+    const { id } = useParams<{ id: string }>();
+    const [product, setProduct] = useState<Product | null>(null);
+    const [loading, setLoading] = useState(true);
+    console.log(id);
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5000/api/products/${id}`)
+            .then((response) => setProduct(response.data))
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false));
+    }, [id]); //será chamado quando o componente montar, ou o valor da dependencia mudar
+
+    if (loading) return <h3>Loading...</h3>;
+
+    if (!product) return <h3>Product not found</h3>;
+
+    return <Typography variant="h2">{product.name}</Typography>;
 }
