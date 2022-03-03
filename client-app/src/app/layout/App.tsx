e25ceaf2f32a054efe9989eec4d603557306ddb0
Mateus Torres <mateustorres67@hotmail.com>
2022-03-03 00:47:52 -0300

    Correção do nome da rota

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -40,7 +40,7 @@ function App() {
             <Container>
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/catalog" component={Catalog} />
-                <Route path="/catalog/:id}" component={ProductDetails} />
+                <Route path="/catalog/:id" component={ProductDetails} />
                 <Route path="/about" component={AboutPage} />
                 <Route path="/contact" component={ContactPage} />
             </Container>
