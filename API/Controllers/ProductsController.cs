a274e9dffd8dffe85332aa5461c649c3e9324e96
Mateus Torres <mateustorres67@hotmail.com>
2022-03-03 22:55:52 -0300

    Correção de namespace + criação de nova controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
-using Controllers.API;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
