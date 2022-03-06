5b4a3ce99cd11a0eab3d8bc2f76f5f1983eabd8d
Mateus Torres <mateustorres67@hotmail.com>
2022-03-06 20:02:32 -0300

    Migration com contrainsts corretas

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Entities
 {
+    [Table("BasketItems")] //isso fará com que o nome da tabela seja esse, ao inves do nome da classe
     public class BasketItem
     {
         public int Id { get; set; }
@@ -14,6 +16,7 @@ namespace API.Entities
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
-
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
     }
 }
