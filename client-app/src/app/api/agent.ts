6d10a6a16b354eabb862d87b254c735ac7c3cde0
Mateus Torres <mateustorres67@hotmail.com>
2022-03-06 01:10:26 -0300

    interceptação dos erros

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
+import { history } from "../..";
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
 
@@ -13,13 +14,24 @@ axios.interceptors.response.use(
         const { data, status } = error.response!;
         switch (status) {
             case 404:
+                if (data.errors) {
+                    const modelStateError: string[] = [];
+                    for (const key in data.errors) {
+                        if (data.errors[key])
+                            modelStateError.push(data.errors[key]);
+                    }
+                    throw modelStateError.flat();
+                }
                 toast.error(data.title);
                 break;
             case 401:
                 toast.error(data.title);
                 break;
             case 500:
-                toast.error(data.title);
+                history.push({
+                    pathname: "/server-error",
+                    state: { error: data },
+                });
                 break;
             default:
                 break;
