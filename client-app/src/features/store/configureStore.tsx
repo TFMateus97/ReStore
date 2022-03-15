734114eebe408b3786a7b283a7fe8256edfedc3e
Mateus Torres <mateustorres67@hotmail.com>
2022-03-14 23:10:38 -0300

    criaçao de catalog slice

--- client-app/src/features/store/configureStore.tsx
+++ client-app/src/features/store/configureStore.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { basketSlice } from "../basket/basketSlice";
+import { catalogSlice } from "../catalog/catalogSlice";
 import { counterSlice } from "../contact/counterSlice";
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
-        basket: basketSlice.reducer
+        basket: basketSlice.reducer,
+        catalog: catalogSlice.reducer
     }
 })
