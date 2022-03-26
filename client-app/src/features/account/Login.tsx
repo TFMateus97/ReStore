55fae323e2142d8933438e32763c75bdcc1a69ab
Mateus Torres <mateustorres67@hotmail.com>
2022-03-25 23:48:11 -0300

    refact de formulario de login

--- client-app/src/features/account/Login.tsx
+++ client-app/src/features/account/Login.tsx
@@ -1,72 +1,63 @@
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme } from '@mui/material/styles';
 import { Paper } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import agent from '../../app/api/agent';
-
-const theme = createTheme();
+import { FieldValues, useForm } from 'react-hook-form';
+import { LoadingButton } from '@mui/lab';
+import { useAppDispatch } from '../contact/counterSlice';
+import { signInUser } from './accountSlice';
 
 export default function Login() {
-    const [values, setValues] = useState({
-        username: '',
-        password: ''
-    });
+    const history = useHistory();
+    const dispatch = useAppDispatch();
+    const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm();
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault(); //prevent full page submit
-    agent.Account.login(values).then(response => console.log(response));
-  };
-
-  function handleInputChange(event: any) {
-      const {name, value} = event.target;
-      setValues({...values, [name]: value});
-  }
+    async function submitForm(data: FieldValues) {
+      await dispatch(signInUser(data));
+      history.push('/catalog')
+    }
 
   return (
       <Container component={Paper} maxWidth="sm" sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', p: 4}} >
-
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               fullWidth
               label="Username"
-              name="username"            
               autoFocus
-              onChange={handleInputChange}
-              value={values.username}
-            />
+              {...register('username', {required: 'Username is required' })}
+              error={!!errors.username}
+              helperText={errors?.username?.message} />
             <TextField
               margin="normal"
               fullWidth
-              name="password"
               label="Password"
               type="password"
               id="password"
-              onChange={handleInputChange}
-              value={values.password}
-                />
-            <Button
+              {...register('password', {required: 'Password is required' })}
+              error={!!errors.password}
+              helperText={errors?.password?.message} />
+            <LoadingButton
+              loading={isSubmitting}
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
               Sign In
-            </Button>
+            </LoadingButton>
             <Grid container>
               <Grid item>
                 <Link to="/register">
