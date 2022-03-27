72dfe21b431b6fd70f607302d8c59f5a96160d11
Mateus Torres <mateustorres67@hotmail.com>
2022-03-26 22:26:32 -0300

    Signed In Menu Component

---
+++ client-app/src/app/layout/SignedInMenu.tsx
@@ -0,0 +1,40 @@
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
+import { signOut } from '../../features/account/accountSlice';
+import { useAppDispatch, useAppSelector } from '../../features/contact/counterSlice';
+
+export default function SignedInMenu() {
+const dispatch = useAppDispatch();
+const {user} = useAppSelector(state => state.account);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button 
+      color='inherit'
+      onClick={handleClick}
+      sx={{typography: 'h6'}}
+      >
+        {user?.email}
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My orders</MenuItem>
+        <MenuItem onClick={() => dispatch(signOut())}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+}
