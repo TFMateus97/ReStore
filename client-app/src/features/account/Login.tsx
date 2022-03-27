07468582eec16aca267083c91d2c63f5a4f53243
Mateus Torres <mateustorres67@hotmail.com>
2022-03-27 15:55:04 -0300

    private routes

--- client-app/src/features/account/Login.tsx
+++ client-app/src/features/account/Login.tsx
@@ -6,7 +6,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Paper } from '@mui/material';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
 import { useAppDispatch } from '../contact/counterSlice';
@@ -15,12 +15,13 @@ import { signInUser } from './accountSlice';
 export default function Login() {
     const history = useHistory();
     const dispatch = useAppDispatch();
+    const location = useLocation<any>();
     const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm();
 
     async function submitForm(data: FieldValues) {
       try {
         await dispatch(signInUser(data));
-        history.push('/catalog')
+        history.push(location.state?.from?.pathname || '/catalog')
       } catch (error: any) {
         console.log(error); 
       }
