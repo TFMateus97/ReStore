6d53cba4d224a864922667efcf40e40c19328edc
Mateus Torres <mateustorres67@hotmail.com>
2022-03-27 22:05:49 -0300

    Order identity + Owned identities

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using API.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using API.Entities.OrderAggregate;
 
 namespace API.Data
 {
@@ -12,6 +13,7 @@ namespace API.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         //nao haverá dbset para basketItem, pois não há necessidade de consultar um item individualmente
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
